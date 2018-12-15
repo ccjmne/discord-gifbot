80f0895b7dade3bdb6cf9ec56bee1e1c0c23d777
ccjmne <ccjmne@gmail.com>
2018-12-16 00:30:24 +0100

    Support single emoji for 'superslap' command

--- commands/slap.js
+++ commands/slap.js
@@ -11,7 +11,7 @@ export default function handle(message) {
   if (matched) {
     const user = message.mentions.users.first() || message.author;
     if (supercharged) {
-      const [left, right] = (([left, right]) => [left || ':right_facing_fist:', right || ':left_facing_fist:'])(emoji.split(/\s+/));
+      const [left, right] = !!emoji.trim() ? (([left, right]) => [left.trim(), right || left.trim()])(emoji.split(/\s+/)) : [':right_facing_fist:', ':left_facing_fist:'];
       return message.channel.send(user + '\n' + [...Array(len).keys()]
         .map(i => ({ prefix: ' '.repeat(i), infix: ' '.repeat(len - i) }))
         .map(({ prefix, infix }) => `${ prefix }${ left }${ infix }:slight_smile:${ infix }${ right }`).join('\n') +
