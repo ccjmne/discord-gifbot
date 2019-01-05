35b6c85f1c9f3190aca02c2baf1ed9416e19f23b
ccjmne <ccjmne@gmail.com>
2019-01-05 16:28:29 +0100

    Rewrite the slap cmd w/ smoother face transition

--- commands/slap.js
+++ commands/slap.js
@@ -2,23 +2,19 @@
 
 import parse from '../utils/parsing';
 
-const len = 4;
+const smileys = [':smile:', ':grin:', ':thinking:', ':worried:', ':dizzy_face:']
+  .map((e, i, a) => ({ face: e, space: ' '.repeat(a.length - i - 1), prefix: ' '.repeat(i) }));
 
 export default function handle(message) {
-  const parsed = parse(message);
-  const [matched, supercharged, emoji] = /^!(super)?slap\s*(.*)/i.exec(parsed) || [];
+  const [matched, supercharged, emoji] = /^!(super)?slap\s*(.*)/i.exec(parse(message)) || [];
 
   if (matched) {
     const user = message.mentions.users.first() || message.author;
     if (supercharged) {
       const [left, right] = !!emoji.trim() ? (([left, right]) => [left.trim(), right || left.trim()])(emoji.split(/\s+/)) : [':right_facing_fist:', ':left_facing_fist:'];
-      return message.channel.send(user + '\n' + [...Array(len).keys()]
-        .map(i => ({ prefix: ' '.repeat(i), infix: ' '.repeat(len - i) }))
-        .map(({ prefix, infix }) => `${ prefix }${ left }${ infix }:slight_smile:${ infix }${ right }`).join('\n') +
-        `\n${ ' '.repeat(len) }${ left }:dizzy_face:${ right }`);
+      return message.channel.send(user + '\n' + smileys.map(s => s.prefix + left + s.space + s.face + s.space + right).join('\n'));
     }
 
-    const actual = emoji.length ? emoji : ':left_facing_fist:';
-    message.channel.send(user + '\n' + [...Array(len).keys()].map(x => `:slight_smile:${ ' '.repeat(len - x) }${ actual }`).join('\n') + `\n:dizzy_face:${ actual }`);
+    message.channel.send(user + '\n' + smileys.map(s => s.face + s.space + (emoji.length ? emoji : ':left_facing_fist:')).join('\n'));
   }
 }
